6a1807397d2e5378259e37e63d700f0af13eeb9a
Adrian <useradik@gmail.com>
2020-11-17 21:17:31 +0100

    changes

---
+++ src/pages/app/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="fifa-list-container">
+        <span class="fifa-list-container__header">
+            Task {{ taskNumber }} - {{ headerLabel }}
+        </span>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            headerLabel: 'Fifa list',
+            taskNumber: 2,
+        };
+    },
+
+    components: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+.fifa-list-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    padding: 50px;
+
+    &__header {
+        font-size: 20px;
+        margin-bottom: 25px;
+    }
+}
+</style>
